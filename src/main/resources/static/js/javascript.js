51ef81d4e3f02632fa3edba9555905eca7bf922b
felix3456789 <thc950303@gmail.com>
2019-02-17 19:03:31 +0800

    userCRUD

--- src/main/resources/static/js/javascript.js
+++ src/main/resources/static/js/javascript.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-	$('#CarParkOwner').hide();
-	$('#userType').change(function() {
-	    $('#' + $(this).val()).show();
-	    console.log('#' + $(this).val());
-	    if($(this).val() == "CarParkOwner"){
-	    	$('#member').hide();
-	    }else{
-	    	$('#CarParkOwner').hide();
-	    }
-	});
+	
+	
 });
