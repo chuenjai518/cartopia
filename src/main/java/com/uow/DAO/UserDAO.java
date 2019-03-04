84b8fd2588c85f7c6e1659bc5cb9220206e343d2
Puko <pukox0@users.noreply.github.com>
2019-03-04 18:39:38 +0800

    ????

--- src/main/java/com/uow/DAO/UserDAO.java
+++ src/main/java/com/uow/DAO/UserDAO.java
@@ -241,7 +241,7 @@ public class UserDAO {
 		driverCarID = db.queryForObject(driverCarSQL, String.class);
 		db.update(sql, carParkID, driverCarID);
 		int fee;
-		String feeSQL = "SELECT Fee FROM CarParkSlotInfo WHERE carParkID = " + carParkID + "and carTypeID = " + carTypeID + ";";
+		String feeSQL = "SELECT Fee FROM CarParkSlotInfo WHERE carParkID = " + carParkID + " and carTypeID = " + carTypeID + ";";
 		fee = db.queryForObject(feeSQL, Integer.class);
 		String setSlotStatusSQL ="UPDATE CarParkSlot SET statusID = 1 WHERE carTypeID = " + carTypeID + " and carParkID = " + carParkID + " and statusID = 0 ORDER BY carParkSlotID LIMIT 1;";
 		db.update(setSlotStatusSQL);
